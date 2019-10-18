c9709f837619d4856308dff859e79deadd77f9db
Cameron Moir <cameronjmoir@gmail.com>
2019-10-18 11:45:40 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const newFormItem = document.querySelector('#new-form-item');
+  newFormItem.addEventListener('submit', handleNewFormItemSubmit);
+})
